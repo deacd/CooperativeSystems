517b4eb05dc57e6a00cd4db99fad7eb142df76de
deacd <66627958+deacd@users.noreply.github.com>
2020-06-08 17:53:11 +0300

    Add files via upload

---
+++ diana.py
@@ -0,0 +1,63 @@
+import networkx
+from routing import *
+import arborescences
+import objective_function_experiments
+import infocom2019_experiments
+import srds2019_experiments
+import dsn2019_experiments
+
+
+G = networkx.Graph()
+G.add_edge('A','B', weight=2 )
+G.add_edge('B','D', weight=4 )
+G.add_edge('A','C', weight=3 )
+G.add_edge('C','D', weight=1 )
+
+"""------------------------------------------------
+#Section1 - gweichtete Kanten u. shortest path mit Dijkstra
+
+print(G.nodes())
+print(G.edges())
+
+print(networkx.shortest_path(G,'A','D', weight='weight', method="dijkstra"))
+
+networkx.draw(G)
+plt.savefig("simple_path.png")
+plt.show()
+"""
+
+"""-----------------------------------------------
+#Section2 - erzeugt in diesem fall 3 .png
+
+graph1 = arborescences.init_k_graph(2, 7)
+arborescences.drawArborescences(graph1, "weighted_graph.png")
+"""
+
+"""-----------------------------------------------
+#Section3 - write graph 0 with default paramenters (see objective_function_experiments.py)
+          - to run it, you have to create a simple file in the projekt, as it is explained in the write_graph method: example experiment-objective-function1_graph_10_0.txt
+       
+objective_function_experiments.write_graphs()
+objective_function_experiments.rep +=1
+"""
+
+"""----------------------------------------------
+#Section4 - write graph 1 with default paramenters (see objective_function_experiments.py)
+          - to run it, you have to create a simple file in the projekt, as it is explained in the write_graph method: example experiment-objective-function1_graph_10_1.txt
+          
+graph1 = arborescences.init_k_graph(2, 7)
+objective_function_experiments.write_graphs()
+graph1 = objective_function_experiments.read_graph(1)
+
+#arboList = arborescenses.get_arborescencr_list(graph1)
+"""
+
+
+
+
+
+
+
+
+
+
